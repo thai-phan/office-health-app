deb7e71ae09df8ca3af62630a0ff70e86f1b8a86
thai-phan <thaiph@gaion.kr>
2023-09-13 20:30:35 +0900

    update UI

--- app/src/main/java/com/sewon/officehealth/Application.kt
+++ app/src/main/java/com/sewon/officehealth/Application.kt
@@ -4,13 +4,18 @@ import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
 
+// TODO: connect server
+
+// TODO: notification
+
+// TODO: background service
+
 @HiltAndroidApp
 class Application : Application() {
-    override fun onCreate() {
-        super.onCreate()
-        if (BuildConfig.DEBUG) {
-            Timber.plant(Timber.DebugTree())
-        }
+  override fun onCreate() {
+    super.onCreate()
+    if (BuildConfig.DEBUG) {
+      Timber.plant(Timber.DebugTree())
     }
-//    TODO: connect server`
+  }
 }
