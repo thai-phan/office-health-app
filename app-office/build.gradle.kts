46d52d5ea5481cfdf261b838e40e29cfa43dc59b
thai-phan <thaiph@gaion.kr>
2023-10-10 11:12:42 +0900

    migrate version catalog

--- app-office/build.gradle.kts
+++ app-office/build.gradle.kts
@@ -45,68 +45,68 @@ android {
 }
 
 dependencies {
-  implementation("androidx.activity:activity-compose:1.7.2")
-  implementation("androidx.appcompat:appcompat:1.6.1")
-  implementation("androidx.annotation:annotation:1.7.0")
-  implementation("androidx.core:core-i18n:1.0.0-alpha01")
-  implementation("androidx.core:core-ktx:1.12.0")
-  implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-  implementation("androidx.constraintlayout:constraintlayout-compose:1.0.1")
-  implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.2")
-  implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
-  implementation("androidx.lifecycle:lifecycle-runtime-compose:2.6.2")
-  implementation("androidx.navigation:navigation-compose:2.7.2")
-  implementation("androidx.navigation:navigation-fragment-ktx:2.7.2")
-  implementation("androidx.navigation:navigation-ui-ktx:2.7.2")
-  implementation("androidx.preference:preference-ktx:1.2.1")
-  implementation("com.google.accompanist:accompanist-permissions:0.32.0")
+  implementation(libs.androidx.activity.compose)
+  implementation(libs.androidx.appcompat)
+  implementation(libs.androidx.annotation)
+  implementation(libs.androidx.core.i18n)
+  implementation(libs.androidx.core.ktx)
+  implementation(libs.androidx.constraintlayout)
+  implementation(libs.androidx.constraintlayout.compose)
+  implementation(libs.androidx.lifecycle.livedata.ktx)
+  implementation(libs.androidx.lifecycle.viewmodel.ktx)
+  implementation(libs.androidx.lifecycle.runtime.compose)
+  implementation(libs.androidx.navigation.compose)
+  implementation(libs.androidx.navigation.fragment.ktx)
+  implementation(libs.androidx.navigation.ui.ktx)
+  implementation(libs.androidx.preference.ktx)
+  implementation(libs.accompanist.permissions)
 
 
   // Hilt dependency injection
-  implementation("com.google.dagger:hilt-android:2.48")
+  implementation(libs.hilt.android)
   // kapt "com.google.dagger:hilt-compiler:2.46.1")
-  kapt("androidx.hilt:hilt-compiler:1.0.0")
-  kapt("com.google.dagger:hilt-android-compiler:2.48")
+  kapt(libs.androidx.hilt.compiler)
+  kapt(libs.hilt.android.compiler)
 
-  implementation("androidx.hilt:hilt-navigation-compose:1.0.0")
+  implementation(libs.androidx.hilt.navigation.compose)
 
 
   // DataStore
   //
   // Preferences DataStore (SharedPreferences like APIs)
-  implementation("androidx.datastore:datastore-preferences:1.0.0")
+  implementation(libs.androidx.datastore.preferences)
   // optional - RxJava3 support
-  implementation("androidx.datastore:datastore-preferences-rxjava3:1.0.0")
+  implementation(libs.androidx.datastore.preferences.rxjava3)
 
   // UI Compose
   //
-  val composeVersion = "1.5.1"
-  implementation("androidx.compose.compiler:compiler:1.5.3")
-  implementation("androidx.compose.material3:material3:1.1.1")
-  implementation("androidx.compose.animation:animation:$composeVersion")
-  implementation("androidx.compose.foundation:foundation:$composeVersion")
-  implementation("androidx.compose.foundation:foundation-layout:$composeVersion")
-  implementation("androidx.compose.material:material-icons-core:$composeVersion")
-  implementation("androidx.compose.material:material-icons-extended:$composeVersion")
-  implementation("androidx.compose.runtime:runtime:$composeVersion")
-  implementation("androidx.compose.runtime:runtime-livedata:$composeVersion")
-  implementation("androidx.compose.ui:ui:$composeVersion")
-  implementation("androidx.compose.ui:ui-util:$composeVersion")
-  implementation("androidx.compose.ui:ui-text:$composeVersion")
-  implementation("androidx.compose.ui:ui-tooling:$composeVersion")
-  implementation("androidx.compose.ui:ui-tooling-preview:$composeVersion")
-  implementation("androidx.compose.ui:ui-viewbinding:$composeVersion")
-  debugImplementation("androidx.compose.ui:ui-tooling:$composeVersion")
+  val composeVersion = "1.5.3"
+  implementation(libs.androidx.compiler)
+  implementation(libs.androidx.material3)
+  implementation(libs.androidx.animation)
+  implementation(libs.androidx.foundation)
+  implementation(libs.androidx.foundation.layout)
+  implementation(libs.androidx.material.icons.core)
+  implementation(libs.androidx.material.icons.extended)
+  implementation(libs.androidx.runtime)
+  implementation(libs.androidx.runtime.livedata)
+  implementation(libs.androidx.ui)
+  implementation(libs.androidx.ui.util)
+  implementation(libs.androidx.ui.text)
+  implementation(libs.androidx.ui.tooling)
+  implementation(libs.androidx.ui.tooling.preview)
+  implementation(libs.androidx.ui.viewbinding)
+  debugImplementation(libs.androidx.compose.ui.ui.tooling)
 
   // Log
   //
-  implementation("com.jakewharton.timber:timber:5.0.1")
+  implementation(libs.timber)
 
 
   // Test
   //
-  testImplementation("junit:junit:4.13.2")
-  androidTestImplementation("androidx.test.ext:junit:1.1.5")
-  androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+  testImplementation(libs.junit)
+  androidTestImplementation(libs.androidx.junit)
+  androidTestImplementation(libs.androidx.espresso.core)
 
 }
